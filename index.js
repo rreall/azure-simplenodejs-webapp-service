07a5b381505696536240cc33551c656e5486a993
Manuel Tomás Ortega Sánchez <m.tomas.ortega@gmail.com>
2020-05-06 17:29:09 +0000

    Update index.js
    Test to change content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World In Course!!!");
+    response.end("Hello World In Course 2!!!");
 });
 
 const port = process.env.PORT || 3000;
